e7960539c16017f78d8c869d2e09dbc567377147
Raphaela Beloto <110538902+Lilabrita@users.noreply.github.com>
2024-05-29 08:58:32 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,25 @@
+const btnAula1 = document.querySelector("#btnTeste");
+const escreve = document.querySelector("#textoHeader")
+btnAula1.onclick = () =>{
+    alert("Jesus é meu Best Friend!")
+}
+const nome = "JESUS!".split("");
+
+const escreveNome = (pos)=>{
+    if(pos < nome.length){
+        let saida = "";
+        for(let i=0; i <= pos; i++){
+         saida += nome[pos];
+         setTimeout(() => {
+             escreve.innerHTML = saida;
+             pos++;
+             escreveNome(pos);
+         }, 1000);
+        }
+    }
+}
+
+(()=>{
+   escreveNome(0);
+})();
+
